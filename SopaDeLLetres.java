acb40c8e797a4772cb24aaaa4be49ecf0712b395
a23alechasan <a23alechasan@inspedralbes.cat>
2024-01-11 12:13:39 +0100

    estructura feta

--- SopaDeLLetres.java
+++ SopaDeLLetres.java
@@ -1,17 +1,52 @@
+import java.util.Scanner;
+
 public class SopaDeLLetres {
     public static void main(String[] args) {
         /*
         (Fer Funcio main) -Alex
         >demanar lletres (min max 100) -Iker (demanarLletres)
         >convertir-les a matriu 10x10 -Marc (lletresAMatriu)
-        >imprimir matriu -Adrian (ImprimirMatriu)
-        >solicitar paraules while (!true)
-        -->descartar si la paraula introduida < 3 caracters i > 10 caracters
-        -->detectar si la paraula introduida es troba (horitzontal)
-        -->detectar si la paraula introduida es troba (vertical)
-        -->pintar paraules de vermell quan son trobades
-        -->identificar si les 5 paraules s'han trobat
-        >while (!true) s'acaba*/
+        >imprimir matriu -Adrian (imprimirMatriu)
+        >solicitar paraules  -Alex (solicitarParaula)
+        while (paraulesContades < 5)
+        -->descartar si la paraula introduida < 3 caracters i > 10 caracters -??? (esValida)
+        -->detectar si la paraula introduida es troba (horitzontal) -??? (detectarHoritzontal)
+        -->detectar si la paraula introduida es troba (vertical) -??? (detectarVertical)
+        -->pintar paraules de vermell quan son trobades -??? (pintarParaules)
+        -->imprimir paraules (noves o no) -Adrian (imprimirMatriu(reciclada))
+        --> augmentar paraulesContades si s'han trobat.
+        >while s'acaba quan s'han trobat més de 5 paraules
+        */
 
+        //System.out.println("Sisplau, introdueix les lletres de la sopa de lletres");
+        //int paraulesContades= 0;
+        //demanarLletres;
+        //lletresAMatriu(lletres);
+        //imprimirMatriu;
+        //while (paraulesContades < 5){
+            //System.out.println("Inrodueix una paraula per comprovar si existeix.");
+            //solicitarParaula;
+            //while (!esValida){
+                //System.out.println("Sisplau, introdueix una paraula vàlida.");
+                //solicitarParaula;
+            //}
+            //detectarHoritzontal(paraula);
+            //detectarVertical(paraula);
+
+            /*
+            Aquestes dues funcions han de fer les següents coses:
+            - Comprovar si la paraula es troba
+            -Extraure les coordenades de la lletra d'inici i la lletra del final
+            en format coordenades[xInici,xFinal][yInici,yFinal]
+             */
+
+            //if (coordenades[0][0] != void){
+                //pintarParaules(coordenades[0[0], coordenades[1][1];
+                //imprimirMatriu(matriu);
+                //paraulesContades++;
+            //} else {
+                //imprimirMatriu(matriu);
+            //}
+        //}
     }
 }
