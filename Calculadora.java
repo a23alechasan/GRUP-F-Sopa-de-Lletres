8b53694b09286db808cede66507eadcf62a2046d
a23alechasan <a23alechasan@inspedralbes.cat>
2024-01-11 11:05:21 +0100

    resum funcions

---
+++ Calculadora.java
@@ -0,0 +1,15 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        /*>demanar lletres
+        >convertir-les a matriu 10x10
+        >imprimir matriu
+        >solicitar paraules while (!true)
+        -->descartar si la paraula introduida < 3 caracters i > 10 caracters
+        -->detectar si la paraula introduida es troba (horitzontal)
+        -->detectar si la paraula introduida es troba (vertical)
+        -->pintar paraules de vermell quan son trobades
+        -->identificar si les 5 paraules s'han trobat
+        >while (!true) s'acaba*/
+
+    }
+}
