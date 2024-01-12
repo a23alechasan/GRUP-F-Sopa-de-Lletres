f9790445acea3df2224a691e7b7454e40abe04b4
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2024-01-12 11:00:51 +0100

    Demanar Lletres

--- SopaDeLLetres.java
+++ SopaDeLLetres.java
@@ -1,17 +1,44 @@
+import java.util.Scanner;
+
 public class SopaDeLLetres {
     public static void main(String[] args) {
-        /*
-        (Fer Funcio main)
-        >demanar lletres
-        >convertir-les a matriu 10x10
-        >imprimir matriu
-        >solicitar paraules while (!true)
-        -->descartar si la paraula introduida < 3 caracters i > 10 caracters
-        -->detectar si la paraula introduida es troba (horitzontal)
-        -->detectar si la paraula introduida es troba (vertical)
-        -->pintar paraules de vermell quan son trobades
-        -->identificar si les 5 paraules s'han trobat
-        >while (!true) s'acaba*/
+        char[][] matriu = demanarLletres();
+        mostrarMatriu(matriu);
+    }
 
+    private static char[][] demanarLletres() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dona 100 lletres seguides per començar el joc:");
+        String lletres = input.nextLine();
+        for(int i = 0; i < 100; i++) {
+            if (lletres.length() != 100) {
+                System.out.println("No hi han 100 lletres, torna a provar");
+                lletres = input.nextLine();
+            }
+
+        }
+        return convertirLletresMatriz(lletres);
+    }
+
+    public static char[][] convertirLletresMatriz(String lletres) {
+        char[][] matriu = new char[10][10];
+        int index = 0;
+
+        for (int f = 0; f < 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                matriu[f][c] = lletres.charAt(index++);
+            }
+        }
+
+        return matriu;
+    }
+
+    public static void mostrarMatriu(char[][] matriu) {
+        for (int f = 0; f < 10; f++) {
+            for (int c = 0; c < 10; c++) {
+                System.out.print(matriu[f][c] + " ");
+            }
+            System.out.println();
+        }
     }
 }
