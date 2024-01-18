555176100512293343f7396d7b1941516a225032
a23alechasan <a23alechasan@inspedralbes.cat>
2024-01-18 09:54:28 +0100

    intentat fer decetar horitzontal

--- SopaDeLLetres.java
+++ SopaDeLLetres.java
@@ -4,9 +4,9 @@ public class SopaDeLLetres {
     public static void main(String[] args){
         System.out.println("Sisplau, introdueix les lletres de la sopa de lletres");
         int paraulesContades = 0;
-        String lletres = demanarLletres();
-        char[] matriu = lletresAMatriu(lletres);
-        imprimirMatriu;
+        //String lletres = demanarLletres();
+        //char[][] matriu = lletresAMatriu(lletres);
+        //imprimirMatriu(lletres);
         while (paraulesContades < 5) {
             System.out.println("Introdueix una paraula per comprovar si existeix.");
             String paraula = solicitarParaula();
@@ -16,8 +16,8 @@ public class SopaDeLLetres {
                 paraula = solicitarParaula();
                 lletresParaula = separarParaula(paraula);
             }
-            detectarHoritzontal = (lletresParaula);
-            detectarVertical = (lletresParaula);
+            //int coordenades[1][1] = detectarHoritzontal(matriu, lletresParaula);
+            //detectarVertical = (lletresParaula);
 
             /*
             Aquestes dues funcions han de fer les següents coses:
@@ -27,11 +27,11 @@ public class SopaDeLLetres {
              */
 
             //if (coordenades[0][0] != void){
-            //pintarParaules(coordenades[0[0], coordenades[1][1];
-            //imprimirMatriu(matriu);
-            //paraulesContades++;
-            //} else {
-            //imprimirMatriu(matriu);
+                //pintarParaules(coordenades[0[0], coordenades[1][1]);
+                //imprimirMatriu(matriu);
+                //paraulesContades++;
+            //} else { 
+                //imprimirMatriu(matriu);
             //}
             //}
         }
@@ -47,5 +47,28 @@ public class SopaDeLLetres {
     }
     public  static boolean esValida(char[] lletres){
         return lletres.length >= 3 && lletres.length <= 10;
+    }
+    public static int[][] detectarHoritzontal(char[][] sopa, char[] paraula){
+        int[][] coordenades = new int[1][1];
+        for (int x = 0; x < 10; x++){
+            for (int y = 0; y < 10; y++){
+                int contador = 0;
+                if (sopa[x][y] == paraula[0]){
+                    for (int i = paraula.length; i > 0; i--){
+                        if (sopa[x+i][y] == paraula[i]){
+                            contador++;
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    if (contador == paraula.length){
+                        coordenades[0][0] = sopa[x][y];
+                        coordenades[1][1] = sopa[x+paraula.length][y];
+                    }
+                }
+            }
+        }
+        return coordenades;
     }
 }
