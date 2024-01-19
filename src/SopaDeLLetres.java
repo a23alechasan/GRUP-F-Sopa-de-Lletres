b0326def9ad26c7875d3c90602288fe9138b72b2
AKakuja <a23adrpetkak@inspedralbes.cat>
2024-01-19 09:49:46 +0100

    mtode buscarParaula

--- src/SopaDeLLetres.java
+++ src/SopaDeLLetres.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class SopaDeLLetres {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner sc = new Scanner(System.in);
+
         /*
         (Fer Funcio main) -Alex
         >demanar lletres (min max 100) -Iker (demanarLletres)
@@ -14,5 +17,52 @@ public class SopaDeLLetres {
         -->identificar si les 5 paraules s'han trobat
         >while (!true) s'acaba*/
 
+
+        char[][] matriu = {
+                {'N', 'A', 'S', 'Q', 'Y', 'C', 'C', 'S', 'O', 'Z'},
+                {'M', 'R', 'W', 'O', 'I', 'H', 'Z', 'N', 'F', 'R'},
+                {'Z', 'U', 'I', 'F', 'E', 'U', 'W', 'B', 'Y', 'T'},
+                {'P', 'B', 'F', 'P', 'C', 'S', 'F', 'Q', 'Q', 'I'},
+                {'P', 'Y', 'T', 'H', 'O', 'N', 'B', 'G', 'V', 'I'},
+                {'J', 'P', 'D', 'D', 'B', 'T', 'N', 'E', 'V', 'W'},
+                {'G', 'G', 'E', 'S', 'O', 'Y', 'P', 'Y', 'T', 'U'},
+                {'F', 'Y', 'U', 'I', 'L', 'E', 'C', 'H', 'A', 'V'},
+                {'V', 'B', 'Z', 'S', 'H', 'X', 'T', 'A', 'J', 'T'},
+                {'D', 'Z', 'X', 'L', 'Y', 'J', 'A', 'V', 'A', 'Q'}
+        };
+    }
+
+    static void Buscarpalabra(char[][] matriu, String palabra) {
+        for (int i = 0; i < matriu.length; i++) {
+            String fila = new String(matriu[i]);
+            if (fila.contains(palabra)) {
+                System.out.println("La palabra " + palabra + "esta en la fila" + i);
+                return;
+            }
+            for (int j = 0; j < matriu[0].length; j++) {
+                String columna = "";
+                for (int k = 0; k < matriu.length; k++) {
+                    columna += matriu[i][j];
+                }
+                if (columna.contains(palabra)) {
+                    System.out.println("La palabra " + palabra + "esta en la columa" + j);
+                    return;
+                }
+            }
+        }
+
+    }
+
+    public void mostrarMatriz(char[][] matriz2) {
+
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(matriz2[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
+
+
